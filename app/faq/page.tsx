f24dc3371225f671d5919e700245c74ffdc47469
forgeandtale-spec <forgeandtale@gmail.com>
2026-04-05 19:13:46 +0800

    创建 page.tsx

---
+++ app/faq/page.tsx
@@ -0,0 +1,9 @@
+export default function FAQ() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>FAQ</h1>
+      <p>Shipping: 4–5 weeks</p>
+      <p>Limited: 200 pieces</p>
+    </div>
+  );
+}
