8bfd1ef807825e6a42f00df8c8711016ca4e3919
forgeandtale-spec <forgeandtale@gmail.com>
2026-04-05 19:13:26 +0800

    创建 page.tsx

---
+++ app/product/page.tsx
@@ -0,0 +1,9 @@
+export default function Product() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Product</h1>
+      <p>Limited to 200 pieces</p>
+      <p>Shipping: 4–5 weeks</p>
+    </div>
+  );
+}
